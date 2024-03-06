cf1cb19c8f714a96893e8bd06c7a37ce674e256c
Abdiel <abdiel2012p@gmail.com>
2024-03-05 21:56:22 -0600

    v1 ready

--- src/app/pages/tasks/tasks.page.ts
+++ src/app/pages/tasks/tasks.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from 'src/app/services/task.service';
 import { Task } from '../../models/task.model';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tasks',
@@ -9,10 +10,11 @@ import { Task } from '../../models/task.model';
 })
 export class TasksPage implements OnInit {
 
-  public newTaskTittle!: string;
+  public newTaskTittle: string = "";
   tasks: Task[] = [];
 
-  constructor(private taskService: TaskService) { }
+  constructor(private taskService: TaskService,
+              private alertCtrl: AlertController) { }
 
   ngOnInit() {
     this.loadTasks();
@@ -24,14 +26,47 @@ export class TasksPage implements OnInit {
 
   addTask(){
     console.log('add');
+    if(!this.newTaskTittle.trim()) return;
+    this.taskService.addTask(this.newTaskTittle);
+    this.newTaskTittle= "";
+    this.loadTasks();
   }
 
-  toggleTaskCompleted(){
+  toggleTaskCompleted(id: string){
     console.log('toggle');
+    this.taskService.toggleTaskCompleted(id);
+    this.loadTasks();
   }
 
-  deleteTask(){
+  deleteTask(id: string){
     console.log('delete');
+    this.taskService.deleteTask(id);
+    this.loadTasks();
   }
 
+  async alertConfirmDelete(idTask: string) {
+    const alert = await this.alertCtrl.create({
+      backdropDismiss: false,
+      header: 'Are you sure that you wish to delete this task?',
+      // subHeader: 'A Sub Header Is Optional',
+      // message: 'A message should be a short, complete sentence.',
+      // buttons: ['Cancel','Open', 'Action'],
+      // Buttons can also be configured as an object (json): (NOTE. For further you can view IONIC docs in ion-alert)
+      buttons: [
+        {
+          text: 'Accept',
+          cssClass: 'alert-button-confirm',
+          handler: () => {console.log('Click on Ok');
+                          this.deleteTask(idTask);}
+        },
+        {
+          text: 'Cancelar',
+          // handler: () => {console.log('Click on Cancel')}
+          role: 'cancel',
+          cssClass: 'alert-button-cancel'
+        }
+      ]
+    });
+    await alert.present();
+  }
 }
